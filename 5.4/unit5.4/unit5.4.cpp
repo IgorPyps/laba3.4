865e8227ad909e2c3b7cfa3c1eaaf8dc8310bad7
IgorPyps <115737998+IgorPyps@users.noreply.github.com>
2022-12-09 02:38:31 +0200

    Add files via upload

---
+++ 5.4/unit5.4/unit5.4.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.4/5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit54
+{
+	TEST_CLASS(unit54)
+	{
+	public:
+		
+
+		TEST_METHOD(TestMethod1)
+		{
+			S0(1, 1);
+			S1(1, 1, 1);
+			S2(1, 1, 1);
+			S3(1, 1, 1, 0);
+			S4(1, 1, 1, 0);
+		}
+	};
+}
